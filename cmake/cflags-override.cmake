1347e68dedbb07eb3f3a17f75fd73888feac4306
Enchan <enchant0130@gmail.com>
2023-12-24 22:48:28 +0900

    [Update] override archive command option

--- cmake/cflags-override.cmake
+++ cmake/cflags-override.cmake
@@ -6,3 +6,7 @@
 # (XC8は.oや.objの代わりに.p1 (中間コードファイル) を出力する)
 set(CMAKE_C_OUTPUT_EXTENSION ".p1")
 set(CMAKE_EXECUTABLE_SUFFIX ".elf")
+
+# アーカイバに渡すオプションがxc8-arと通常のarとで異なるため、ここで上書き
+set(CMAKE_C_CREATE_STATIC_LIBRARY)
+string(APPEND CMAKE_C_CREATE_STATIC_LIBRARY "<CMAKE_AR> -r <TARGET> <OBJECTS> <LINK_LIBRARIES>")
