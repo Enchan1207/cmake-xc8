d931e9f0c44f97ac303c92f33d7e1b91a4e691f7
Enchan <enchant0130@gmail.com>
2023-12-25 11:54:24 +0900

    [Update #1] add debug-and-flash target, debug target

--- cmake/find_mplabplatform.cmake
+++ cmake/find_mplabplatform.cmake
@@ -6,7 +6,7 @@
 cmake_minimum_required(VERSION 3.10)
 
 set(MPLAB_PLATFORM_FOUND 0)
-set(MPLABX_INSTALL_DIR "" CACHE PATH "Path to root of IPE (optional)")
+set(MPLABX_INSTALL_DIR "" CACHE PATH "Path to root of MPLAB X IDE (optional)")
 
 # OSごとにインストール先が異なるので、指定がなければ自動で決定する
 if(NOT MPLABX_INSTALL_DIR)
@@ -29,12 +29,15 @@ if(NOT EXISTS ${MPLABX_INSTALL_DIR})
 endif()
 
 # 最新版を取得
-file(GLOB IPE_VARIANTS ${MPLABX_INSTALL_DIR}/v*)
-list(SORT IPE_VARIANTS ORDER DESCENDING)
-list(GET IPE_VARIANTS 0 MPLABX_ROOT)
+file(GLOB MPLAB_X_VARIANTS ${MPLABX_INSTALL_DIR}/v*)
+list(SORT MPLAB_X_VARIANTS ORDER DESCENDING)
+list(GET MPLAB_X_VARIANTS 0 MPLABX_ROOT)
+
+# IPEとmdbのディレクトリパスを特定
 set(IPE_ROOT ${MPLABX_ROOT}/mplab_platform/mplab_ipe)
 set(MDB_ROOT ${MPLABX_ROOT}/mplab_platform/bin)
 
+# 存在チェック
 if(NOT EXISTS ${IPE_ROOT})
     message(WARNING "No any available IPE variants found.")
     message(WARNING "The toolchain expects it to be placed at ${IPE_ROOT}")
